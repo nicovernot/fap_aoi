9448de27c70b4d395b7c372b4df10c4d60a3d043
nicovernot <nicovernot@gmail.com>
2019-07-04 10:33:51 +0200

    paie11

--- src/Controller/PaiementController.php
+++ src/Controller/PaiementController.php
@@ -20,7 +20,9 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use GuzzleHttp\ClientInterface;
-
+use App\Entity\Paiement;
+use App\Entity\Abonnement;
+use App\Repository\AbonnementRepository;
 class PaiementController extends AbstractController
 {
  
@@ -36,13 +38,14 @@ class PaiementController extends AbstractController
 
     $defaultData = ['message' => 'Merci de remplir tous les champs pour éffectuer le paiement'];
     $form = $this->createFormBuilder($defaultData)
-        ->add('UUID', HiddenType::class, ['data' => 'UUIDabcdef',])
+        
+        ->add('AMOUNT', HiddenType::class, ['data' => '0',])
         ->add('CID', HiddenType::class, ['data' => 'CIDabcdef',])
         ->add('CARDNUMBER', TextType::class, [
             'label' => 'Carte de Credit',
             'constraints' => [            
                 new NotBlank(),
-                new Length(['min' => 12,'max' =>12]),
+                new Length(['min' => 10,'max' =>10]),
             ],
         ])
         ->add('MONTH', IntegerType::class, [
@@ -69,16 +72,14 @@ class PaiementController extends AbstractController
          //     var_dump($form);
         if ($form->isSubmitted() && $form->isValid()) {
             $logger->info('We are logging! paiments host request');
-            $logger->info($host);
             $data = $form->getData();
             $uuid="8e39f14a-d44e-51bb-8782-89723e586aa5";
-            //$client = new Client('http://ec2-52-47-88-142.eu-west-3.compute.amazonaws.com:6543');
-            //$req = $client->get('/cardpay/8e39f14a-d44e-51bb-8782-89723e586aa5/test0001/012345678912/1/2019/20');
-            //$client = new Client('127.0.0.1:8000');
-           
+            $logger->info($data['CID']);
             $c = curl_init();
             /*On indique à curl quelle url on souhaite télécharger*/
-            curl_setopt($c, CURLOPT_URL, "http://ec2-52-47-88-142.eu-west-3.compute.amazonaws.com:6543/cardpay/8e39f14a-d44e-51bb-8782-89723e586aa5/test0001/0123456789/1/2019/20");
+            $strdata = "http://ec2-52-47-88-142.eu-west-3.compute.amazonaws.com:6543/cardpay/{$uuid}/{$data['CID']}/{$data['CARDNUMBER']}/{$data['MONTH']}/{$data['YEAR']}/{$data['AMOUNT']}";
+            curl_setopt($c, CURLOPT_URL, "$strdata");
+//            curl_setopt($c, CURLOPT_URL, "http://ec2-52-47-88-142.eu-west-3.compute.amazonaws.com:6543/cardpay/$uuid/$data['CID']/$data['CARDNUMBER']/$data['MONTH']/$data['YEAR']/$data['AMOUNT']");
             /*On indique à curl de nous retourner le contenu de la requête plutôt que de l'afficher*/
             curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
             /*On indique à curl de ne pas retourner les headers http de la réponse dans la chaine de retour*/
@@ -87,16 +88,13 @@ class PaiementController extends AbstractController
             $resp = curl_exec($c);
             // Close request to clear up some resources
             curl_close($c);
-            
-            //$client = new GuzzleHttp\Client(['base_uri' => 'http://127.0.0.1:8000/api/']);
-            // Send a request to https://foo.com/api/test
-            //$response = $client->request('GET', 'users');
             $logger->info($resp);
-            
-            //$httpClient = HttpClient::create();
-           // $response = $httpClient->request('GET', 'http://ec2-52-47-88-142.eu-west-3.compute.amazonaws.com:6543/cardpay/8e39f14a-d44e-51bb-8782-89723e586aa5/test000/012345678912/1/2019/20');
-           // return new RedirectResponse('http://ec2-52-47-88-142.eu-west-3.compute.amazonaws.com:6543/cardpay/8e39f14a-d44e-51bb-8782-89723e586aa5/test000/012345678912/1/2019/20');
-          return $this->render('test.html.twig', ['number' => $resp]);
+ 
+          return $this->render('test.html.twig', ['number' => $resp,
+          'str'=>$strdata,
+         
+          ]
+        );
         }
 
         return $this->render('paiement/index.html.twig', [
@@ -112,12 +110,26 @@ class PaiementController extends AbstractController
      * @Route("/paiement")
      * @Method({"POST"})
      */
-    public function newAction(Request $request,LoggerInterface $logger)
+    public function newAction(Request $request,LoggerInterface $logger,AbonnementRepository $abonnementRepository)
     {
 
         $data = $request->getContent();
         $logger->info('We are logging paiement iii!');
         $logger->info($data);
+
+        $usr = $this->getUser();
+        $today = new \DateTime(); 
+        $idabo =  substr($data['cid'],0,strpos($data['cid'], "-"));     
+        $abbo = $abonnementRepository->findBy(['id' => $idabo]); 
+        $paiement = new Paiement();
+        $paiement->setClient($usr);
+        $paiement->setDate($today);
+        $paiement->setIdpaiement($data['transaction']);
+        $paiement->setAbonnement($abbo[0]);
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($paiement);
+        $entityManager->flush();
+
         return new Response($data);
     }
 
