db1f1ca78570ae58ba0696fd8c70ae05d2df6e7b
nico <nicovernot@gmail.com>
2020-07-02 08:43:33 +0200

    mise a journotifications mercure

--- src/Entity/Place.php
+++ src/Entity/Place.php
@@ -101,7 +101,7 @@ class Place
     }
     public function __toString()
     {
-        $place_nom  = $this->getNom();  
+        $place_nom  = $this->getNom()."_".$this->getTypeprojet();  
 
 
         return $place_nom ?: '';
